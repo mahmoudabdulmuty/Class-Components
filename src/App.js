463d3025f42e33ca17b0a91aa2bec8e7161da33f
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-01 14:54:46 +0200

    State in class components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import { Component } from 'react';
-import Greeting from './components/Greeting';
-import Header from './components/Header';
-
-// // export default function App(props) {
-// //     return (
-// //         <h1>{props.type} component</h1>
-// //     )
-// // }
-
-// export default class App extends Component {
-// 	render() {
-// 		return <h1>{this.props.type} Component</h1>;
-// 	}
-// }
-
-///////////////////////////////////////////////////////////
-
-/*
-
-Challenge:
-1. Convert all 3 components to be class-based
-2. Fix the small bug
-
-*/
-
-// #1
-// function App() {
-//     return (
-//         <div>
-//             <Header />
-//             <Greeting />
-//         </div>
-//     )
-// }
 
 export default class App extends Component {
+	state = {
+		goOut: 'Yes'
+	};
+
+	toggleGoOut() {
+		// setGoOut((prevState) => {
+		// 	return prevState === 'Yes' ? 'No' : 'Yes';
+		// });
+	}
 	render() {
 		return (
-			<div>
-				<Header username="Mahmoud" />
-				<Greeting />
+			<div className="state">
+				<h1 className="state--title">Should I go out tonight?</h1>
+				<div className="state--value" onClick={this.toggleGoOut}>
+					<h1>{this.state.goOut}</h1>
+				</div>
 			</div>
 		);
 	}

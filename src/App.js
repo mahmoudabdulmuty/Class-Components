16c885af9894f99e9eb58b2dd8b471be2a3a59af
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-01 22:41:46 +0200

    Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -1,36 +1,132 @@
-import { Component } from 'react';
+import React from 'react';
 
-export default class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			count: 0
-		};
-		this.add = this.add.bind(this);
-		this.subtract = this.subtract.bind(this);
-	}
+class App extends React.Component {
+	state = JSON.parse(localStorage.getItem('formData')) || {
+		firstName: '',
+		lastName: '',
+		email: '',
+		comments: '',
+		employment: '',
+		favColor: '',
+		isFriendly: true
+	};
 
-	add() {
-		this.setState((prevState) => ({ count: prevState.count + 1 }));
-	}
+	handleChange = (event) => {
+		const { name, value, type, checked } = event.target;
+		type === 'checkbox'
+			? this.setState({
+					[name]: checked
+			  })
+			: this.setState({
+					[name]: value
+			  });
+	};
 
-	subtract() {
-		this.setState((prevState) => ({ count: prevState.count - 1 }));
+	componentDidUpdate() {
+		console.log('Update');
+		localStorage.setItem('formData', JSON.stringify(this.state));
 	}
 
 	render() {
+		console.log('render');
 		return (
-			<div className="counter">
-				<button className="counter--minus" onClick={this.subtract}>
-					–
-				</button>
-				<div className="counter--count">
-					<h1>{this.state.count}</h1>
-				</div>
-				<button className="counter--plus" onClick={this.add}>
-					+
-				</button>
-			</div>
+			<form>
+				<input
+					type="text"
+					placeholder="First Name"
+					onChange={this.handleChange}
+					name="firstName"
+					value={this.state.firstName}
+				/>
+				<input
+					type="text"
+					placeholder="Last Name"
+					onChange={this.handleChange}
+					name="lastName"
+					value={this.state.lastName}
+				/>
+				<input
+					type="email"
+					placeholder="Email"
+					onChange={this.handleChange}
+					name="email"
+					value={this.state.email}
+				/>
+				<textarea
+					value={this.state.comments}
+					placeholder="Comments"
+					onChange={this.handleChange}
+					name="comments"
+				/>
+				<input
+					type="checkbox"
+					id="isFriendly"
+					checked={this.state.isFriendly}
+					onChange={this.handleChange}
+					name="isFriendly"
+				/>
+				<label htmlFor="isFriendly">Are you friendly?</label>
+				<br />
+				<br />
+
+				<fieldset>
+					<legend>Current employment status</legend>
+					<input
+						type="radio"
+						id="unemployed"
+						name="employment"
+						value="unemployed"
+						checked={this.state.employment === 'unemployed'}
+						onChange={this.handleChange}
+					/>
+					<label htmlFor="unemployed">Unemployed</label>
+					<br />
+
+					<input
+						type="radio"
+						id="part-time"
+						name="employment"
+						value="part-time"
+						checked={this.state.employment === 'part-time'}
+						onChange={this.handleChange}
+					/>
+					<label htmlFor="part-time">Part-time</label>
+					<br />
+
+					<input
+						type="radio"
+						id="full-time"
+						name="employment"
+						value="full-time"
+						checked={this.state.employment === 'full-time'}
+						onChange={this.handleChange}
+					/>
+					<label htmlFor="full-time">Full-time</label>
+					<br />
+				</fieldset>
+				<br />
+
+				<label className="favColor" htmlFor="favColor">
+					What is your favorite color?
+				</label>
+				<br />
+				<select
+					id="favColor"
+					value={this.state.favColor}
+					onChange={this.handleChange}
+					name="favColor"
+				>
+					<option value="red">Red</option>
+					<option value="orange">Orange</option>
+					<option value="yellow">Yellow</option>
+					<option value="green">Green</option>
+					<option value="blue">Blue</option>
+					<option value="indigo">Indigo</option>
+					<option value="violet">Violet</option>
+				</select>
+			</form>
 		);
 	}
 }
+
+export default App;
